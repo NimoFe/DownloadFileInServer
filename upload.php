291e9831e96194225ac14ad45263cf75530a3d08
Nimo <nfediakin@mail.ru>
2022-10-14 18:09:57 +0500

    upload

--- upload.php
+++ upload.php
@@ -2,14 +2,18 @@
 if (!empty($_FILES['attachment'])) {
     $file = $_FILES['attachment'];
 
-// собираем путь до нового файла - папка uploads в текущей директории
-// в качестве имени оставляем исходное файла имя во время загрузки в браузере
     $srcFileName = $file['name'];
     $newFilePath = __DIR__ . '/uploads/' . $srcFileName;
 
-    if (file_exists($newFilePath)) {
+    $allowedExtensions = ['jpg', 'png', 'gif'];
+    $extension = pathinfo($srcFileName, PATHINFO_EXTENSION);
+    if (!in_array($extension, $allowedExtensions)) {
+        $error = 'Загрузка файлов с таким расширением запрещена!';
+    } elseif ($file['error'] !== UPLOAD_ERR_OK) {
+        $error = 'Ошибка при загрузке файла.';
+    } elseif (file_exists($newFilePath)) {
         $error = 'Файл с таким именем уже существует';
-    }elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
+    } elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
         $error = 'Ошибка при загрузке файла';
     } else {
         $result = 'http://myproject.loc/uploads/' . $srcFileName;
